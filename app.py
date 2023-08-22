bf335c5d479c9d751103cc0407194e78f76975a0
farhan0167 <afi225@nyu.edu>
2023-08-22 13:46:45 -0400

    new branch

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 from mta_api.stationtimes import StationTimes
 from mta_api.stations import Stations
 import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -18,11 +20,14 @@ def get_station_time_by_id(stop_id):
     sBound = stop['south_bound_trains']
     sBound.sort()
 
-    return stop
+    return jsonify({
+        'statusCode': 200,
+        'data': stop
+    })
 @app.route("/api/get-station-details")
 def get_station_details():
     stations = Stations().stations
-    return stations
+    return jsonify(stations)
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
