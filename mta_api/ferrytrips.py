f0a37eda49a8a5e1a9d9652530820862cb2d0164
farhan0167 <afi225@nyu.edu>
2023-08-22 13:47:45 -0400

    new branch

---
+++ mta_api/ferrytrips.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+class FerryTrips:
+  def __init__(self, csv_path='./trips.txt'):
+    self.csv_path = csv_path
+    self.df_trips = self.make_df()
+  
+  def make_df(self):
+    df = pd.read_csv(self.csv_path)
+    df.columns = df.columns.str.replace(' ', '_')
+    return df
+
+  def get_trips(self):
+    trips = {}
+    for row in self.df_trips.itertuples():
+      trips[row.trip_id] = {
+          'trip_id': row.trip_id,
+          'service_id': row.service_id,
+          'trip_headsign': row.trip_headsign
+      }
+      #stations.append(station_data)
+    return trips
